19f63d460a575a984e04f44d6da2ebd2be83301b
razaali0123 <aliiqbal0123@gmail.com>
2022-08-22 17:15:05 +0200

    api working

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+
+
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=a19d7946ea240c2c92f7d5fb6813b3f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
+    return response.json()
