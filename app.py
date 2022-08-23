9f868ddba9974d555b0868e73062fd86b44221d6
razaali0123 <aliiqbal0123@gmail.com>
2022-08-23 13:17:56 +0200

    added env variables

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -11,7 +15,7 @@ def get_movie_names(num_pages):
     movie_name = []
     movie_id = []
     for page_num in range(int(num_pages)):
-        url = "https://api.themoviedb.org/3/discover/movie?api_key=a19d7946ea240c2c92f7d5fb6813b3f6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+ str(page_num+1) +"&with_watch_monetization_types=flatrate"
+        url = "https://api.themoviedb.org/3/discover/movie?api_key="+ str(os.getenv("api_key")) +"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+ str(page_num+1) +"&with_watch_monetization_types=flatrate"
         response = requests.get(url).json()
         res = response['results']
 
@@ -27,13 +31,12 @@ def recommendations():
     recommended = []
     content = request.json
     for id in content['ids']:
-        url = "https://api.themoviedb.org/3/movie/" + str(id) + "/recommendations?api_key=a19d7946ea240c2c92f7d5fb6813b3f6&language=en-US&page=1"
+        url = "https://api.themoviedb.org/3/movie/" + str(id) + "/recommendations?api_key=" + str(os.getenv("api_key")) + "&language=en-US&page=1"
         response_post = requests.get(url).json()
         res_post = response_post["results"]
         res_post = res_post[0]
         recommended.append(res_post['title'])
     return jsonify({"Recommend_movies": recommended})
-    return response_post
 
 
 
